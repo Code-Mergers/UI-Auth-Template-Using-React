49742d3d4d1ef0bd381e3e68433c5c438a963ec7
Salokya Kumar <kumar.salokya007@gmail.com>
2021-07-15 08:48:58 +0530

    added preloader

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,7 +2,7 @@ import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/database";
 import "firebase/firestore";
-import config from './env';
+import config from '../env';
 
 if (!firebase.apps.length) {
   //initializing with the config object
